520fc0bb2c0bb606cd6801e7b253dc3a81fa5b9b
Harshal Abdulla <harshalabdulla1999@gmail.com>
2021-09-03 19:42:26 +0530

    Concatination

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-//const is like let but it make it constant and cant vary the value/overwrite
-//opeartors
-const a =2
-const b = 15
-//arithmetic operators
-console.log(a+b)
+const a = '2'
+const b = 2
 
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b) //reminder
-console.log(a ** b) //exponent = 2^15
-//assignment operators
-let c = 0
-
-c++ //c+1
-c += 7 //increment by 7
-c *=5 //multiply by 5
-c /= 3  //divide by 3
-//line by line interpretation occurs so it overwrites the value before that line
-console.log(c)
-
-
-
+console.log(a + b) //typecasting and concatinationg here '2' is a string
 
 
 
