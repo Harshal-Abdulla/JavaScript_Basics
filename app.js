bf89c61521e26c5c6349d75396331c34d6f6d9ed
Harshal Abdulla <harshalabdulla1999@gmail.com>
2021-09-03 18:18:32 +0530

    Data types

---
+++ app.js
@@ -0,0 +1,45 @@
+// Data types
+// Primitive data types - float, number etc
+// semi colon is not neccesasry
+var Name = "Harshal"
+Name = "Abdulla"
+console.log(Name)
+
+//it overwrites the first one
+
+//either a var or let to declare a variable
+
+let age = 19;
+console.log(age)
+
+console.log(typeof age)
+
+let name = 'asdgkj'
+console.log(typeof name)
+//everything in a single quotes is a string
+
+let brand
+console.log(brand)
+//undefined is when no value is given to the variable
+
+
+let isTrue = true
+console.log(isTrue)
+
+
+
+// Non primitive data type
+
+let arr = [1,2,3,45,55]
+console.log(arr)
+arr.push('value')
+arr.unshift(false) //pushes to the beginning
+console.log(arr)
+console.log(typeof arr)
+
+
+
+
+
+
+
