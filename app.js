738c7586b838ef6a0ddf88eedfa781ec997e136a
Harshal Abdulla <harshalabdulla1999@gmail.com>
2021-09-05 22:50:44 +0530

    DOM Selectors

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("HI")
+// const element = document.getElementById('select')
+// const element = document.getElementsByClassName('heading')
+// const element = document.getElementsByTagName('h2')
+//check google for doubts
+//queryselector is the main it can be used in many cases and use css to select
+
+const element = document.querySelector('h3.heading')
+console.log(element)
