d9d08056d39fcd3b8b00b45c067f839d4f78942b
Harshal Abdulla <harshalabdulla1999@gmail.com>
2021-09-06 11:52:27 +0530

    styles and classList

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-// const element = document.getElementById('select')
-// const element = document.getElementsByClassName('heading')
-// const element = document.getElementsByTagName('h2')
-//check google for doubts
-//queryselector is the main it can be used in many cases and use css to select
+ const element = document.querySelector('#select')
+// element.innerHTML = "This is from JS"
+// element.style.color = "cyan"
+// element.style.fonrSize = "40px"
+// element.style = 'display:none'
 
-const element = document.querySelector('h3.heading')
-console.log(element)
+// console.log(element.classList)
+// element.classList.add('five')
+// console.log(element.classList)
+// element.classList.remove('one')
+// console.log(element.classList)
+
+const doc = document.querySelector('body')
+console.log(doc.classList.toggle('dark'))
+
+//the above will toggle if else fuction so we can add or remove the dark mode is given in the body if {dothis} else (dothat ) this is done here
+
+const value = element.classList.contains('six')
+
+console.log(value)
+//this allows you to check whether the class is present or not
