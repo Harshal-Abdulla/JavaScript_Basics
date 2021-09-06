668d681a01beb6822674c1a6e48930e86a0144d6
Harshal Abdulla <harshalabdulla1999@gmail.com>
2021-09-06 12:37:42 +0530

    Event Listeners

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
- const element = document.querySelector('#select')
-// element.innerHTML = "This is from JS"
-// element.style.color = "cyan"
-// element.style.fonrSize = "40px"
-// element.style = 'display:none'
+const button = document.querySelector('button')
+const body = document.querySelector('body')
 
-// console.log(element.classList)
-// element.classList.add('five')
-// console.log(element.classList)
-// element.classList.remove('one')
-// console.log(element.classList)
+// button.addEventListener('click', () => {console.log('you clicked me')})
+//addEventListener will add events lik if we click say the console shows you clicked you can make it hover, double click etc ctrl and space shows  many is there
 
-const doc = document.querySelector('body')
-console.log(doc.classList.toggle('dark'))
-
-//the above will toggle if else fuction so we can add or remove the dark mode is given in the body if {dothis} else (dothat ) this is done here
-
-const value = element.classList.contains('six')
-
-console.log(value)
-//this allows you to check whether the class is present or not
+button.addEventListener('dblclick', ()=> {
+    body.classList.toggle('dark')
+})
